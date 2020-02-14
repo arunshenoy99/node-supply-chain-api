b1a84efb9d79e1598a29f0d503c545b26c55be9c
Arun <devarunshenoy99@gmail.com>
2020-02-14 18:50:33 +0530

    added new route to support item search

--- src/models/item.js
+++ src/models/item.js
@@ -9,6 +9,7 @@ const itemSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
+        lowercase: true,
         unique: true,
     },
     description: {
--- src/routers/items.js
+++ src/routers/items.js
@@ -5,6 +5,7 @@ const Warehouse = require('../models/warehouse')
 
 const router = new express.Router()
 
+//Create a new item
 router.post('/items', auth, async (req, res) => {
     if (!req.user.supplier) {
         return res.status(400).send()
@@ -30,6 +31,7 @@ router.post('/items', auth, async (req, res) => {
     }
 })
 
+//Get the item barcode
 router.get('/items/:id/barcode', async (req, res) => {
     try {
         const item = await Item.findById(req.params.id)
@@ -38,6 +40,23 @@ router.get('/items/:id/barcode', async (req, res) => {
         }
         res.setHeader('Content-Type', 'image/png')
         res.send(item.barcode)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+//Search for an item
+router.get('/items', async (req, res) => {
+    try {
+        const itemKeys = Object.keys(req.query)
+        itemKeys.forEach((itemKey) => {
+            req.query[itemKey] = req.query[itemKey].toLowerCase()
+        })
+        const items = await Item.find(req.query)
+        if (items.length === 0) {
+            return res.status(404).send()
+        }
+        res.send(items)
     } catch (e) {
         res.status(400).send()
     }
